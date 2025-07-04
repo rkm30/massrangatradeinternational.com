9c780da73388f1667a78c45115cfc3a91121d598
rkm30 <rashedkhanmilon98@gmail.com>
2025-07-04 09:32:46 +0600

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", padding: "2rem" }}>
+      <h1>Massranga Trade International</h1>
+      <p>Coming Soon...</p>
+    </div>
+  );
+}
